e25a6da892f95a16d46781a728bccaaa0dfe6874
Rakha-arch <miftahuzahran@gmail.com>
2025-05-26 14:30:09 +0700

    menambahkan pasien ke daftar pasien

--- ASOSIATION/asosiation.cpp
+++ ASOSIATION/asosiation.cpp
@@ -46,7 +46,8 @@ void pasien :: cetakDokter () {
 }
 
 void dokter::tambahPasien (pasien* pPasien) {
-   
+    daftar_pasien.push_back(pPasien);
+pPasien->tambahDokter(this);
 }
 void dokter:: cetakPasien() {
    
